30f17ecdfd8e0b0cbd16937e8cec9649611d4377
vladimir-mhl <vladimir.mhl@gmail.com>
2015-12-28 17:55:20 +0200

    fixed build and packaging

---
+++ src/CallfireApiClient.Tests/Utilities/ZipTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.IO;
+using System.Linq;
+using Ionic.Zip;
+
+namespace CallfireApiClient.Tests.Utilities
+{
+    public class ZipTask : Task
+    {
+        [Required]
+        public ITaskItem[] Files { get; set; }
+
+        [Required]
+        public string ZipFileName { get; set; }
+
+        public override bool Execute()
+        {
+            var fileNames = Files.Select(f => f.ItemSpec);
+            Log.LogMessage(MessageImportance.Normal, "Package files into {0}", ZipFileName);
+            if (File.Exists(ZipFileName))
+            {
+                File.Delete(ZipFileName);
+            }
+            var zip = new ZipFile(ZipFileName);
+            foreach (var fileName in fileNames)
+            {
+                zip.AddFile(fileName, "/");
+            }
+            zip.Save();
+            return true;
+        }
+    }
+}
+
