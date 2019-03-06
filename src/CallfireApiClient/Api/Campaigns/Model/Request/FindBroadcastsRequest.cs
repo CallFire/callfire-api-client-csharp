840d14d185a47042c7ca75cd792ac6eb265b4924
vmalinovskiy <vmalinovskiy@callfire.com>
2019-03-06 14:06:47 +0200

    vmalinovskiy - added date interval filtering parameters to broadcasts lookup

--- src/CallfireApiClient/Api/Campaigns/Model/Request/FindBroadcastsRequest.cs
+++ src/CallfireApiClient/Api/Campaigns/Model/Request/FindBroadcastsRequest.cs
@@ -11,10 +11,16 @@ namespace CallfireApiClient.Api.Campaigns.Model.Request
 
         public bool? Running { get; set; }
 
+        public bool? Scheduled { get; set; }
+
+        public DateTime? IntervalBegin { get; set; }
+
+        public DateTime? IntervalEnd { get; set; }
+
         public override string ToString()
         {
-            return string.Format("[{0} FindBroadcastsRequest: Label={1}, Name={2}, Running={3}]", 
-                base.ToString(), Label, Name, Running);
+            return string.Format("[{0} FindBroadcastsRequest: Label={1}, Name={2}, Running={3}, Scheduled={4}, intervalBegin={5}, intervalEnd={6}]", 
+                base.ToString(), Label, Name, Running, IntervalBegin, IntervalEnd);
         }
     }
 }
